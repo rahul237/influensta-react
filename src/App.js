3fa0de6af856361e5a031a1847bbbd59ebc0e945
Rahul Mehta <rahulbhushanmehta@gmail.com>
2020-12-04 01:49:39 +0530

    added my campaigns with css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Brand from './Brand/Brand';
+import Campaigns from './components/brand/my_campaigns/my_campaigns'
 
 function App() {
   return (
     <div className="App">
         <Brand name="frewf" age="34"></Brand>
+        <Campaigns/>
     </div>
   );
 }
---
+++ src/components/brand/my_campaigns/my_campaigns.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './my_campaigns.css';
+const my_campaigns = (props) => {
+    return (
+    <p className="my_campaigns">
+        These are all my campaigns
+    </p>
+    ) 
+}
+
+export default my_campaigns;
