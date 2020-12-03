6ce96d8f40e2b6df4f3ef25728a8aaf0a8cedcc4
Rahul Mehta <rahulbhushanmehta@gmail.com>
2020-12-04 01:40:17 +0530

    testing app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Brand from './Brand/Brand';
 
 function App() {
   return (
     <div className="App">
-        Hey There!
+        <Brand name="frewf" age="34"></Brand>
     </div>
   );
 }
