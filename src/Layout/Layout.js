c7e63c449e08a38a519255efca8428f12ea17622
rahul237 <rahulbhushanmehta@gmail.com>
2020-12-05 20:26:21 +0530

    added layout

---
+++ src/Layout/Layout.js
@@ -0,0 +1,16 @@
+import react from 'react';
+
+import Aux from '../hoc/aux';
+
+const layout = (props)=>(
+    <Aux>
+    <div>
+        Toolbar,Sidedrawer, Backdrop
+    </div>
+    <main>
+        {props.children}
+    </main>
+    </Aux>
+);
+
+export default layout;
